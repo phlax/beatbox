f0e9c92007ed42ed4b56ddfc6f216ea89b6157e2
Ryan Northey <ryan@synca.io>
2020-06-30 14:13:37 +0100

    ctx

--- context/bin/entrypoint.sh
+++ context/bin/entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
 
-echo "Fetching latest beats src"
-cd "$GOPATH/src/github/elastic/beats"
-git fetch origin
-git rebase "origin/$BEATS_BRANCH"
-
 if [ -d /var/lib/beatbox/extra ]; then
     echo "COPYING EXTRA BEATS..."
 fi
